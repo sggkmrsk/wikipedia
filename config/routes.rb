46dab452a0f58567c76896429a1da15e36ff0db0
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-02-04 20:08:37 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to:"top#index"
   resources :users, only: [:index, :edit, :update]
   resources :top ,only: :index
+  resources :howtos ,only: :index
   resources :pages ,only: [:index,:new,:show,:create,:edit,:update,:destroy] do
     resources :contents ,only: [:new,:edit,:create,:update,:destroy]
     resources :comments ,only: [:new,:create,:destroy]

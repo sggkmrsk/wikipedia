50de9f7deaa72bed6c61356915e5a2d2af4b4d5b
sggkmrsk <57355040+sggkmrsk@users.noreply.github.com>
2020-04-26 19:44:54 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,42 @@
 require 'rails_helper'
-describe User do
+RSpec.describe User ,type: :model do
   describe '#create' do
+    let(:user) {build_stubbed(:user)}
+    it "is valid with a name, email, password, password_confirmation" do
+      expect(user).to be_valid
+    end
     it "is invalid without a name" do
-      user = User.new(name: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-      user.valid?
-      expect(user.errors[:name]).to include("can't be blank")
+      user.name = ""
+      expect(user.errors[:name]).to include("can't be blank") if user.valid?
     end
     it "is invalid without a email" do
-      user = User.new(name: "aaa", email: "", password: "00000000", password_confirmation: "00000000")
-      user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      user.email = ""
+      expect(user.errors[:email]).to include("can't be blank") if user.valid?
+    end
+    it "is invalid without a password" do
+      user.password = ""
+      expect(user.errors[:password]).to include("can't be blank") if user.valid?
+    end
+    it "is invalid without a password_confirmation" do
+      user.password_confirmation = ""
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password") if user.valid?
+    end
+    it "is invaild without match password to password_confirmation" do
+      user.password_confirmation = "0000001"
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password") if user.valid?
+    end
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user)
+      expect(another_user.errors[:email]).to include("has already been taken") if another_user.valid?
+    end
+    it "is valid with a password that has more than 6 characters" do
+      user.password = "000000" && user.password_confirmation = "000000"
+      expect(user).to be_valid
+    end
+    it "is invalid with a password that has less than 6 characters" do
+      user.password = "00000" && user.password_confirmation = "00000"
+      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)") if user.valid?
     end
   end
 end
